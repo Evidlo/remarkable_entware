4ec7f5c66029f3ab4855d6c0e531fa531c9b7792
evan <evan@evanw.org>
2019-03-21 20:44:34 -0500

    use new entware repo

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 This is a modified installer for [Entware](https://github.com/Entware/Entware), a lightweight package manager and software repo for embedded devices.
 
-See a list of available packages [here](http://pkg.entware.net/binaries/armv7).
+See a list of available packages [here](http://bin.entware.net/armv7sf-k3.2/).
 
 # Installation
 
 Connect your device to the internet before executing.
 
 ``` bash
-scp entware_install.sh opt.mount root@10.11.99.1:
+scp entware_install.sh root@10.11.99.1:
 ssh root@10.11.99.1 ./entware_install.sh
 ```
 
 All entware data is located in `/opt` (which actually points to `/home/root/.entware` because of size constraints on the root partition).  Base installation is ~13MB.
 
-# Example
+# Examples
 
 ``` bash
 opkg install git
--- entware_install.sh
+++ entware_install.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
+# Evan Widloski - 2019-03-21
+# Modified Entware installer from http://bin.entware.net/armv7sf-k3.2/installer/generic.sh
+
+set -e
+cleanup() {
+    echo "Encountered error.  Cleaning up and quitting..."
+    # get out of /opt so it can be unmounted
+    cd /home/root
+
+    if [ -d /opt ]
+    then
+        umount /opt
+        rm /opt -rf
+    fi
+
+    if [ -d /home/root/.entware ]
+    then
+        rm /home/root/.entware -rf
+    fi
+
+    if [ -f /etc/systemd/system/opt.mount ]
+    then
+        rm /etc/systemd/system/opt.mount
+    fi
+}
+trap cleanup ERR
+
+unset LD_LIBRARY_PATH
+unset LD_PRELOAD
 
 echo "Info: Checking for prerequisites and creating folders..."
 
@@ -7,21 +36,41 @@ then
     echo "Error: Folder /opt exists! Quitting..."
     exit
 else
-    mkdir /opt
-
     if [ -d /home/root/.entware ]
     then
         echo "Error: Folder /home/root/.entware exists! Quitting..."
         exit
     else
+        mkdir /opt
         # mount /opt in /home for more storage space
         mkdir -p /home/root/.entware
         mount --bind /home/root/.entware /opt
     fi
 fi
 
+# create systemd mount unit to mount over /opt on reboot
+cat >/etc/systemd/system/opt.mount <<EOF
+[Unit]
+Description=Bind mount over /opt to give entware more space
+DefaultDependencies=no
+Conflicts=umount.target
+Before=local-fs.target umount.target
 
-for folder in bin etc/init.d lib/opkg sbin share tmp usr var/log var/lock var/run
+[Mount]
+What=/home/root/.entware
+Where=/opt
+Type=none
+Options=bind
+
+[Install]
+WantedBy=local-fs.target
+EOF
+systemctl daemon-reload
+systemctl enable opt.mount
+
+
+# no need to create many folders. entware-opt package creates most
+for folder in bin etc lib/opkg tmp var/lock
 do
   if [ -d "/opt/$folder" ]
   then
@@ -33,43 +82,48 @@ do
 done
 
 echo "Info: Opkg package manager deployment..."
-CURARCH="armv7"
 DLOADER="ld-linux.so.3"
-URL=http://pkg.entware.net/binaries/$CURARCH/installer
+URL=http://bin.entware.net/armv7sf-k3.2/installer
 wget $URL/opkg -O /opt/bin/opkg
-chmod +x /opt/bin/opkg
+chmod 755 /opt/bin/opkg
 wget $URL/opkg.conf -O /opt/etc/opkg.conf
-wget $URL/ld-2.23.so -O /opt/lib/ld-2.23.so
-wget $URL/libc-2.23.so -O/opt/lib/libc-2.23.so
-wget $URL/libgcc_s.so.1.2.23 -O /opt/lib/libgcc_s.so.1
+wget $URL/ld-2.27.so -O /opt/lib/ld-2.27.so
+wget $URL/libc-2.27.so -O /opt/lib/libc-2.27.so
+wget $URL/libgcc_s.so.1 -O /opt/lib/libgcc_s.so.1
+wget $URL/libpthread-2.27.so -O /opt/lib/libpthread-2.27.so
 cd /opt/lib
-chmod +x ld-2.23.so
-ln -s ld-2.23.so $DLOADER
-ln -s libc-2.23.so libc.so.6
+chmod 755 ld-2.27.so
+ln -s ld-2.27.so $DLOADER
+ln -s libc-2.27.so libc.so.6
+ln -s libpthread-2.27.so libpthread.so.0
 
 echo "Info: Basic packages installation..."
 /opt/bin/opkg update
 /opt/bin/opkg install entware-opt
-if [ ! -f /opt/usr/lib/locale/locale-archive ]
-then
-        wget http://pkg.entware.net/binaries/other/locale-archive.2.23 -O /opt/usr/lib/locale/locale-archive
-fi
 
-# from Entware (2018)
+# Fix for multiuser environment
+chmod 777 /opt/tmp
+
 # now try create symlinks - it is a std installation
 if [ -f /etc/passwd ]
 then
     ln -sf /etc/passwd /opt/etc/passwd
+else
+    cp /opt/etc/passwd.1 /opt/etc/passwd
 fi
 
 if [ -f /etc/group ]
 then
     ln -sf /etc/group /opt/etc/group
+else
+    cp /opt/etc/group.1 /opt/etc/group
 fi
 
 if [ -f /etc/shells ]
 then
     ln -sf /etc/shells /opt/etc/shells
+else
+    cp /opt/etc/shells.1 /opt/etc/shells
 fi
 
 if [ -f /etc/shadow ]
@@ -87,16 +141,8 @@ then
     ln -sf /etc/localtime /opt/etc/localtime
 fi
 
-# Upgrading to Entware
-/opt/bin/opkg update
-/opt/bin/opkg upgrade
 
-# create systemd mount unit to mount over /opt on reboot
-parent_dir=$(dirname $0)
-cp $parent_dir/opt.mount /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable opt.mount
-
-echo "Info: Congratulations!"
-echo "Info: If there are no errors above then Entware has been installed."
-echo "Info: Add /opt/bin & /opt/sbin to your PATH variable"
+echo ""
+echo "Info: Congratulations! Entware has been installed."
+echo "Info: Add /opt/bin & /opt/sbin to your PATH by executing"
+echo 'ssh root@10.11.99.1 echo '\\\'\''PATH=$PATH:/opt/bin:/opt/sbin'\'\\\'\'' >> ~/.bashrc'\'
